fc6017cb88c3c6b606c0e3429533c174cf640d8b
Geo-the-Pitbull <92966244+Geo-the-Pitbull@users.noreply.github.com>
2022-08-14 21:19:47 -0500

    The LetsGoShopping App.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
+import { ItemDetailsComponent } from './components/item-details/item-details.component';
+import { ItemsListComponent } from './components/items-list/items-list.component';
+import { AddCategorieComponent } from './components/add-categorie/add-categorie.component';
+import { CategorieDetailsComponent } from './components/categorie-details/categorie-details.component';
+import { CategoriesListComponent } from './components/categories-list/categories-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddItemComponent,
+    ItemDetailsComponent,
+    ItemsListComponent,
+    AddCategorieComponent,
+    CategorieDetailsComponent,
+    CategoriesListComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
